f64cd842913bb9bfc8ebc948ee3fd658e002ee98
zaba221 <36647938+zaba221@users.noreply.github.com>
2020-09-22 23:01:18 +0200

    Update SeleniumTest.java

--- src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
+++ src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
