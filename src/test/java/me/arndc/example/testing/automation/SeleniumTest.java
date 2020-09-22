86d8c6aecd4132b612272bc9401f321ceb0e6e8e
zaba221 <36647938+zaba221@users.noreply.github.com>
2020-09-22 22:59:47 +0200

    Update SeleniumTest.java

--- src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
+++ src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
@@ -28,7 +28,16 @@ public class SeleniumTest {
 
     @Before
     public void setUp() {
-        driver = new ChromeDriver();
+        
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized"); // open Browser in maximized mode
+        options.addArguments("disable-infobars"); // disabling infobars
+        options.addArguments("--disable-extensions"); // disabling extensions
+        options.addArguments("--disable-gpu"); // applicable to windows os only
+        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        options.addArguments("--no-sandbox"); // Bypass OS security model
+        driver = new ChromeDriver(options);
+        
     }
 
     @Test
