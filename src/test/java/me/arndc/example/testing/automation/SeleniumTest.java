0e7f1e1eafb2d212cc04f88429e778502f5a70e9
zaba221 <36647938+zaba221@users.noreply.github.com>
2020-09-22 21:15:24 +0200

    Update SeleniumTest.java

--- src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
+++ src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
@@ -22,7 +22,7 @@ public class SeleniumTest {
 
     @BeforeClass
     public static void start() {
-        final String webDriverPath =  "driver/chromedriver.exe";
+        final String webDriverPath =  "home/vagrant/chromedriver.exe";
         System.setProperty("webdriver.chrome.driver", webDriverPath);
     }
 
