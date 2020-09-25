acd986e31989f74976cce1bb9d2ba47353c5b8ef
lamansHenix <54840385+lamansHenix@users.noreply.github.com>
2020-09-25 12:17:48 +0200

    Update SeleniumTest.java

--- src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
+++ src/test/java/me/arndc/example/testing/automation/SeleniumTest.java
@@ -44,7 +44,7 @@ public class SeleniumTest {
     @Test
     public void test() throws InterruptedException {
         
-    	driver.get("http://192.168.2.10:8080");
+    	driver.get("http://192.168.4.28:8086");
     	
     	assertTrue(driver.getTitle().equalsIgnoreCase("Spring Demo Project"));
 
